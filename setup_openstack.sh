7fa197825ad387f7ccaedd189ea1e2221ff797da
Henryk Paluch <hpaluch@seznam.cz>
2023-11-17 11:02:37 +0000

    Install and setup MySQL for OpenStack

--- setup_openstack.sh
+++ setup_openstack.sh
@@ -12,8 +12,10 @@ echo "HOST='$HOST' HOST_IP='$HOST_IP' Release='$RELEASE'"
 
 CFG_BASE=$HOME/.config/osfs
 CFG_STAGE_DIR=$CFG_BASE/stages
+CFG_SEC_DIR=$CFG_BASE/secrets
+MYSQL_ROOT_PWD_FILE=$CFG_SEC_DIR/mysql_root_pwd.txt
 
-for i in $CFG_BASE $CFG_STAGE_DIR
+for i in $CFG_BASE $CFG_STAGE_DIR $CFG_SEC_DIR
 do
 	[ -d "$i" ] || mkdir -vp "$i"
 done
@@ -25,5 +27,58 @@ STAGE=$CFG_STAGE_DIR/001basepkg
 	sudo apt-get install -y software-properties-common eatmydata curl wget jq
 	touch $STAGE
 }
+
+STAGE=$CFG_STAGE_DIR/002enable-os-repo
+[ -f $STAGE ] || {
+	sudo add-apt-repository cloud-archive:$RELEASE
+	touch $STAGE
+}
+
+STAGE=$CFG_STAGE_DIR/003install-mysql
+[ -f $STAGE ] || {
+	sudo apt-get install -y mariadb-server python3-pymysql 
+	touch $STAGE
+}
+
+STAGE=$CFG_STAGE_DIR/004config-mysql
+[ -f $STAGE ] || {
+sudo tee /etc/mysql/mariadb.conf.d/99-openstack.cnf  <<EOF
+[mysqld]
+bind-address = $HOST_IP
+
+default-storage-engine = innodb
+innodb_file_per_table = on
+max_connections = 4096
+collation-server = utf8_general_ci
+character-set-server = utf8
+# my additions
+innodb_buffer_pool_size = 256m
+# see: https://mariadb.com/docs/reference/es/system-variables/innodb_flush_log_at_trx_commit/
+innodb_flush_log_at_trx_commit = 0
+EOF
+	sudo systemctl restart mariadb
+	touch $STAGE
+}
+
+STAGE=$CFG_STAGE_DIR/005secure-mysql
+[ -f $STAGE ] || {
+	echo "Anser no to socket and note MySQL root password you set"
+	sudo mysql_secure_installation
+	echo "Now store MySQL root password to $MYSQL_ROOT_PWD_FILE"
+	touch $STAGE
+}
+
+# Verify that MySQL root password file exists
+[ -r "$MYSQL_ROOT_PWD_FILE" ] || {
+	echo "ERROR: You have to store MySQL root password to $MYSQL_ROOT_PWD_FILE" >&2
+	exit 1
+}
+
+# Verify that we can login to MySQL as root user
+mysql -u root -p`cat $MYSQL_ROOT_PWD_FILE` -e 'show databases' || {
+	echo "ERROR: Unable to login as root to MySQL" >&2
+	exit 1
+}
+
 
 exit 0
